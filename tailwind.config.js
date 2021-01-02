4283e2cc78dd9e3f0fce690b4eb14720b6ca74e1
hoondeveloper <hoonskyn@naver.com>
2021-01-03 03:14:00 +0900

    feat: TailwindCSS, PostCSS, twin.macro 적용

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundOpacity: ['dark'],
+      textOpacity: ['dark'],
+    },
+  },
+  plugins: [],
+};
