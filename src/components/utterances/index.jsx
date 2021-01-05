73d1618f152e74e3fa98f4c1f7aec029240d0238
hoondeveloper <hoonskyn@naver.com>
2021-01-06 01:56:52 +0900

    feat: Utterances 개선
    - 테마 변경될 때마다 다른 테마의 Utterances 보여주도록 구현
    - Utterances iframe 로딩되기 전 Lottie 출력

--- src/components/utterances/index.jsx
+++ src/components/utterances/index.jsx
@@ -1,7 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './index.scss';
 import { THEME } from '../../constants';
 import ThemeContext from '../../contexts/theme';
+import classNames from 'classnames';
+import Lottie from 'react-lottie';
+import loadingAnimationData from 'content/assets/lotties/refresh.json';
+
+import * as Dom from '../../utils/dom';
 
 const src = 'https://utteranc.es/client.js';
 const branch = 'master';
@@ -9,32 +14,63 @@ const DARK_THEME = 'photon-dark';
 const LIGHT_THEME = 'github-light';
 
 export const Utterances = ({ repo }) => {
-  const rootElm = React.createRef();
+  const lightElmRef = React.createRef();
+  const darkElmRef = React.createRef();
+
   const { state } = useContext(ThemeContext);
 
-  useEffect(() => {
-    while (rootElm.current.hasChildNodes()) {
-      rootElm.current.removeChild(rootElm.current.firstChild);
-    }
+  const [isLoaded, setIsLoaded] = useState(true);
 
-    const isDarkTheme = state?.theme === THEME.DARK;
-    const utterances = document.createElement('script');
+  const loadingOptions = {
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimationData,
+  };
+
+  useEffect(() => {
+    Dom.getElement('.utterances');
+
+    const lightElm = document.createElement('script');
+    const darkElm = document.createElement('script');
     const utterancesConfig = {
       src,
       repo,
       branch,
-      theme: isDarkTheme ? DARK_THEME : LIGHT_THEME,
       label: 'comment',
       async: true,
       'issue-term': 'pathname',
       crossorigin: 'anonymous',
     };
+    lightElm.setAttribute('theme', LIGHT_THEME);
+    darkElm.setAttribute('theme', DARK_THEME);
 
     Object.keys(utterancesConfig).forEach(configKey => {
-      utterances.setAttribute(configKey, utterancesConfig[configKey]);
+      lightElm.setAttribute(configKey, utterancesConfig[configKey]);
+      darkElm.setAttribute(configKey, utterancesConfig[configKey]);
     });
-    rootElm.current.appendChild(utterances);
-  }, [state]);
 
-  return <div className="utterances" ref={rootElm} />;
+    lightElmRef.current.appendChild(lightElm);
+    darkElmRef.current.appendChild(darkElm);
+
+    darkElmRef.current.addEventListener('DOMNodeInserted', () => {
+      const utterancesIframe = Dom?.getElement('.utterances')?.querySelector('iframe');
+
+      const callback = () => {
+        setIsLoaded(false);
+        utterancesIframe.removeEventListener('load', callback);
+      };
+
+      setIsLoaded(true);
+      utterancesIframe?.addEventListener('load', callback);
+    });
+  }, []);
+
+  return (
+    <>
+      {isLoaded && <Lottie options={loadingOptions} height={200} width={200} />}
+      <div className={classNames({ hidden: state?.theme === THEME.LIGHT })} ref={darkElmRef} />
+      <div className={classNames({ hidden: state?.theme === THEME.DARK })} ref={lightElmRef} />
+    </>
+  );
 };
